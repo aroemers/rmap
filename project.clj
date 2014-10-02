77bd1f798549f08a0119e3dc6247231aa0ae79e1
aroemers <github@company.romeinszoon.nl>
2014-10-02 23:26:55 +0200

    Set version number

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rmap "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject rmap "0.1"
+  :description "A Clojure library designed to define literal lazy, recursive maps."
+  :url "http://github.com/aroemers/rmap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
